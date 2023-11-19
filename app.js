e6e31d313868e67094a54fb2add1130f951ce23d
RATTHAPHUM KHAMKAEO <nont@RATTHAPHUMs-MacBook-Air.local>
2023-11-19 21:04:39 +0700

    first hello my name's NONT

---
+++ app.js
@@ -0,0 +1,13 @@
+document.write ("ratthaphum")
+alert ("Welcome");
+
+let a = "RATTHAPHUM"
+let b = 500;
+let number1=1, number2=2, number3=3;
+let number = Array(1,2,3,4,5,6,7,8,9,10);
+let color = ["แดง","ดำ","น้ำเงิน","เหลือง"];
+
+console.log(typeof(b));
+console.log("คัวเลข",number);
+console.log("index 5 =",number[5]);
+console.log(color);
